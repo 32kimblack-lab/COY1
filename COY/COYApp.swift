1ee493bea00f110bb73cb622a8ae4cf1a9298952
ShekinaHappuc <124217861+ShekinaHappuc@users.noreply.github.com>
2025-11-16 01:27:39 -0500

    Initial Commit

---
+++ COY/COYApp.swift
@@ -0,0 +1,17 @@
+//
+//  COYApp.swift
+//  COY
+//
+//  Created by Shekina Happuc on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct COYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
